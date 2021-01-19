d0e91ad752198fe5ba0b00e4db0c37ff39967bb3
AlexVKO <ale@alexvko.com>
2021-01-18 19:50:31 -0500

    add some specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,16 +3,19 @@ require 'rails_helper'
 RSpec.describe User do
   describe "#generate_jwt" do
     it "generates a valid jwt with a payload of id" do
+      sleep 1 + rand
     end
   end
 
   describe "#personal_data" do
     it "returns data of first_name, last_name, and email" do
+      sleep 1 + rand
     end
   end
 
   describe "#subscription" do
     it "returns the associated subscription" do
+      sleep 1 + rand
     end
   end
 end
